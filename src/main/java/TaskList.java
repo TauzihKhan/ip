8c635807b74eecd8e9d41290dc25b9d993226b9c
TauzihKhan <travistowntopia@gmail.com>
2026-08-19 22:31:46 +0800

    Changed TaskList to use dynamic arraylist instead of static primitive array

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,49 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Stores up to 100 tasks.
  */
 public class TaskList {
   private static final int MAX_SIZE = 100;
 
-  private final Task[] itemList;
-  private int size;
+  private final List<Task> itemList;
 
   public TaskList() {
-    itemList = new Task[MAX_SIZE];
-    size = 0;
+    itemList = new ArrayList<>(MAX_SIZE);
   }
 
   public Task get(int index) {
-    return itemList[index];
+    return itemList.get(index);
   }
 
   public int size() {
-    return size;
+    return itemList.size();
   }
 
   /**
-   * Adds an item to the fixed-size task array.
+   * Adds an item to the task list.
    *
    * @throws PotatoBotException if the task array is already full
    */
   public void add(Task item) throws PotatoBotException {
-    if (size >= MAX_SIZE) {
+    if (itemList.size() >= MAX_SIZE) {
       throw new PotatoBotException("Your potato sack is full! It can only hold 100 items.");
     }
 
-    itemList[size] = item;
-    size++;
+    itemList.add(item);
   }
 
   public boolean isEmpty() {
-    return size == 0;
+    return itemList.isEmpty();
   }
 
   public void markDone(int index) {
-    itemList[index].markDone();
+    itemList.get(index).markDone();
   }
 
   public void markReset(int index) {
-    itemList[index].markReset();
+    itemList.get(index).markReset();
   }
 
   public String printList() {
@@ -52,8 +52,8 @@ public class TaskList {
     }
 
     String message = "Here are the tasks in your list:";
-    for (int i = 0; i < size; i++) {
-      message += "\n  " + (i + 1) + ".[" + itemList[i].getStatusIcon() + "] " + itemList[i];
+    for (int i = 0; i < itemList.size(); i++) {
+      message += "\n  " + (i + 1) + ".[" + itemList.get(i).getStatusIcon() + "] " + itemList.get(i);
     }
     return message;
   }
